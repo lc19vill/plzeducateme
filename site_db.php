76804deb810ad6d0012be920df333f6237f3979b
Luis Villa <32991509+lc19vill@users.noreply.github.com>
2019-03-17 07:25:41 -0400

    Add files via upload

---
+++ site_db.php
@@ -0,0 +1,24 @@
+<?
+	
+/* -----------------------------------------------------------------------------
+Connects to server, executes SQL statement, closes connection
+and returns MySQLi result object  	
+
+Input: SQL Statement (string)
+Output: MySQLi result object (iterator) 	
+----------------------------------------------------------------------------- */
+function run_query($sql) {
+  #$mysqli = new mysqli('localhost', 'sienasel_p_admin', 'Pages2018!', 'sienasel_pages');
+  $mysqli = new mysqli('104.154.215.21', 'bk30davi', '$HackRPI2019', 'plzeducateme-sql-instance');
+  if ($mysqli->connect_errno) {
+    die('Failed to connect to MySQL: '.$mysqli->connect_error);
+  }	
+  $result = $mysqli->query($sql);
+  if (!$result) {
+    die('Error executing query: '.$mysqli->error.'<br> SQL: '.$sql);
+  }
+  $mysqli->close();
+  return $result;
+}	
+		
+?>
